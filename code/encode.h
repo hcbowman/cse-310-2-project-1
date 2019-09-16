0572314d0fc3e6eca05af5907fd1f4e395479194
Hunter Bowman <hbowman877@hotmail.com>
2019-09-16 00:04:25 -0700

    working on data block

--- code/encode.h
+++ code/encode.h
@@ -3,23 +3,28 @@
 
 #include <vector>
 
+#define DATA_BLOCK_SIZE 32768
+
 class Encode {
 
 	private:
 		int charCount;
+		std::vector<std::vector<char>> dataBlock(DATA_BLOCK_SIZE, std::vector<char>(DATA_BLOCK_SIZE, 0));
+
 
 	public:
 		Encode() {
-			std::vector<std::vector<char>> v();
+			dataBlock;
 			charCount = 0;
 		}
 
 		Encode(int n) {
-			std::vector<std::vector<char>> v(n);
+			dataBlock;
 			charCount = n;
 		}
 
-
+		void set(std::string inpLine);
+		std::string get();
 		void shift();
 		void sort();
 
